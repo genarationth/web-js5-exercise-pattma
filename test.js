f58b3e26504e4ec4f9cf534e9d29c46b7e40da16
patt02 <pattarin.sine.m@gmail.com>
2023-03-15 22:18:50 +0700

    Arrays and Loops exercise

---
+++ test.js
@@ -0,0 +1,66 @@
+// // Exercise #1
+// // Part 1
+let person = ['Sofia','David','Juan'];
+console.log(person);
+
+// // Part 2
+person.push('Sara','Augustin');
+console.log(person);
+console.log(person[0]);
+
+// // Part 3
+person.splice(2,0,'Renata');
+console.log(person);
+person.push('Elena');
+console.log(person);
+
+// Exercise #2
+let height = 5;
+let temp = '';
+for (let i = 1; i <= height; i++) {
+    for (let j = 0; j < i; j++) {
+        temp += '*' + ' ';
+    }
+    temp += '\n';
+}
+console.log(temp);
+
+// Exercise #3
+// ===== 1 =====
+let xValue = 10;
+let x = 0.5;
+while (x < xValue) {
+    if (xValue > 0) {
+        xValue -= x;
+        console.log(xValue);
+        xValue--;
+    }
+}
+
+// ===== 2 =====
+let num = 1;
+while (num < 100) {
+	console.log(num);
+    num += 2;
+} 
+
+// ===== 3 =====
+let loopNum = (num) => {
+    let i = 1;
+    while (i <= num) {
+        let x = i;
+        console.log(`[${x}]`);
+        i++;
+    }
+}
+loopNum(6);
+
+// ===== 4 =====
+let n = 5;
+let sum = 0;
+let i = 1;
+while (i <= n) {
+    sum += i;
+    i++;
+}
+console.log(sum);
